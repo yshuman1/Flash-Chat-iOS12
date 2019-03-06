b52fe681c9a496952f8358475e53663fdec8f1f6
Yasin Shuman <email@yasin.io>
2019-03-06 04:14:51 -0800

    added custom component :)

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import Firebase
 
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+  
     
     // Declare instance variables here
 
@@ -26,7 +28,8 @@ class ChatViewController: UIViewController {
         super.viewDidLoad()
         
         //TODO: Set yourself as the delegate and datasource here:
-        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
         
         
         //TODO: Set yourself as the delegate of the text field here:
@@ -38,8 +41,8 @@ class ChatViewController: UIViewController {
         
 
         //TODO: Register your MessageCell.xib file here:
-
-        
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        configureTableView()
     }
 
     ///////////////////////////////////////////
@@ -50,18 +53,33 @@ class ChatViewController: UIViewController {
     
     //TODO: Declare cellForRowAtIndexPath here:
     
+  
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        let messageArray = ["First Message", "Second Message", "Third Message", "supeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeer lonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnng!"]
+        
+        cell.messageBody.text = messageArray[indexPath.row]
+        return cell
+    }
     
     //TODO: Declare numberOfRowsInSection here:
     
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
     
     //TODO: Declare tableViewTapped here:
     
     
     
     //TODO: Declare configureTableView here:
-    
+    func configureTableView(){
+        messageTableView.rowHeight = UITableView.automaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+    }
     
     
     ///////////////////////////////////////////
@@ -107,8 +125,13 @@ class ChatViewController: UIViewController {
     @IBAction func logOutPressed(_ sender: AnyObject) {
         
         //TODO: Log out the user and send them back to WelcomeViewController
-        
-        
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        }
+        catch {
+            print("error, there was a problem signing out")
+        }
     }
     
 
--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -6,6 +6,7 @@
 
 
 import UIKit
+import Firebase
 
 
 class LogInViewController: UIViewController {
@@ -28,7 +29,15 @@ class LogInViewController: UIViewController {
 
         
         //TODO: Log in the user
-        
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            
+            if error != nil {
+                print(error)
+            } else {
+                print("login successful!")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
     }
     
